ddfce7aabe9c8e75a2a0287a3e3912becc1f287e
Keerthana-Sai <saikeerthana.yanamala@gmail.com>
2024-02-06 21:09:55 +0530

    added files

---
+++ LOOPS/src/Ascci.java
@@ -0,0 +1,9 @@
+public class Ascci {public static void main(String args[]){
+    for(char i='A';i<='Z';i++){
+        int AscciValue=(int) i;
+        System.out.println(i + ":" + AscciValue);
+
+    }
+}
+    
+}
---
+++ LOOPS/src/SumOfSeries.java
@@ -0,0 +1,13 @@
+public class SumOfSeries {
+    public static void main(String args[]){
+        int series=10;
+       int  sum=0;
+        for(int i=1;i<=series;i++){
+            sum =sum+i;
+
+
+        }
+        System.out.println(sum);
+    }
+    
+}
